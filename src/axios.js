b6e7ee37a216b96e0ad4d87acaa682e6ca8ce022
Prakash7708 <bprakash2902@gmail.com>
2022-11-25 21:41:35 +0530

    render-server

--- src/axios.js
+++ src/axios.js
@@ -2,7 +2,8 @@ import axios from "axios";
 
 let instance=axios.create({
      //baseURL:"http://localhost:3001"
-     baseURL:"https://splice-food.herokuapp.com/"
+     //baseURL:"https://splice-food.herokuapp.com/"
+     baseURL:"https://spilcefood-server.onrender.com/"
 })
 
 export default instance;
